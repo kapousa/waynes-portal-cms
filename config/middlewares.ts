5417e77c49c487735ec28f42b898f304d949d6fb
Hatem <gadallah.hatem@gmail.com>
2026-01-21 07:40:45 +0400

    Enabling open file from articles in iframe in the portal

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,22 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'localhost:1337'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'localhost:1337'],
+          // CTO FIX: Allow your portal to embed Strapi content
+          'frame-ancestors': ["'self'", 'http://localhost:8080'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
